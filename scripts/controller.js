4100a0067cb01762f77a8dd6e28dcedbbcb66f78
Aimee Sohn <aimee.sohn@proximity.com.sg>
2014-02-20 17:32:18 +0800

    update age gate

--- scripts/controller.js
+++ scripts/controller.js
@@ -9,13 +9,13 @@ app
     }
 ])
 .controller('AgeGateCtrl',['$scope', '$location',
-	function($scope, $location) {
+    function($scope, $location) {
         // Added this at the moment only for page sliding effect
-		$scope.submit = function() {
+        $scope.submit = function() {
             $location.path('/landing');
         };
         $scope.muslim = 'false';
-	}
+    }
 ])
 .controller('LandingPageCtrl', ['$scope', '$timeout', 
     function($scope, $timeout) {
@@ -47,24 +47,25 @@ app
             // Mobile!
         } else {
             // Not mobile
-            console.log('not mobile');
+            console.log('not mobile, landing page will load video');
             
             $timeout(function() {
-                console.log('timeout');
+                console.log('timeout, calling function func()');
                 func();
 
             }, 300);
-
         }
-
     }
 ])
-.controller('MadeofmorePageCtrl', ['$scope', 
-	function($scope) {
+.controller('MadeofmorePageCtrl', ['$scope',
+    function($scope) {
         $scope.playfilm = function() {
             console.log('playfilm');
         }
-        
+        // jsonp
+        // $http.jsonp(""+'&callback=JSON_CALLBACK')      
+
+        // infinite loading effect
         $scope.items = [];    
         var counter = 0;
 
@@ -76,17 +77,18 @@ app
         };
         
         $scope.loadingPost();
-
-	}
+    }
 ])
 .controller('AgeGateBlockCtrl',['$scope',
     function($scope) {
 
     }
 ])
-.controller('RedirectingCtrl',['$scope',
-    function($scope) {
-
+.controller('RedirectingCtrl',['$timeout', '$location', 
+    function($timeout, $location) {
+        $timeout(function() {
+            $location.path('/landing'); 
+        }, 3000);
     }
 ])
 .controller('BehindTheSceneCtrl', ['$scope',
@@ -94,24 +96,163 @@ app
 
     }
 ])
-.controller('TestCtrl', ['$scope', '$location', '$timeout',
-    function($scope, $location , $timeout ) {
+.controller('TestCtrl', ['$scope', '$location', '$timeout', '$http',
+    function($scope, $location , $timeout, $http) {
+
+
+        // initial value for the listing country and muslim checkbox
         $scope.country = 'Country';
-
-        $scope.submitMobile = function() {
-            $location.path('/landing');
-        };
-
-        $scope.submit = function() {
-            $location.path('/landing');
-        };
-
         $scope.muslim = 'false';
 
-        $scope.click = function() {
+        // when user is muslim, redirecting to blocking page
+        $scope.muslimTrue = function() {
             $timeout(function() {
                 $location.path('/age-gate-block'); 
             }, 1000);
         }
+
+        $scope.submitMobile = function() {
+            $location.path('/landing');
+        };;
+
+        // Getting countries list for both desktop and mobile
+        $http.get('age_country.json').then(function(res) {
+            $scope.countries = res.data;
+        });
+
+        /* 
+         * submitting for MOBILE from here , all comment should be deleted
+         */
+
+        // submit agegate form - mobile
+        $scope.submitFormMobile = function(legalAge, mbday) {
+            // getting legal age and user's bday info
+            var val = mbday.split("-");
+            var day = parseInt(val[2]);
+            var month = parseInt(val[1] - 1);
+            var year = parseInt(val[0]);
+            var min_age = legalAge;
+
+            console.log('born in ',parseInt(val[0]));
+
+            var userLegalYear = parseInt(val[0]) + parseInt(min_age);
+
+            console.log('m.legal year', userLegalYear);
+
+            var theirDate = new Date((userLegalYear), month, day);
+            var today = new Date;
+            var thisYear = today.getYear() + 1900;
+
+            console.log('m.exact legal date',new Date((userLegalYear), month, day));
+
+            if((today.getYear() - theirDate.getYear()) < 0 ) {
+                console.log('drink fanta');
+                $timeout(function() {
+                    $location.path('/age-gate-block'); 
+                }, 1000);
+            } else if (today.getYear() - theirDate.getYear() == 0 ) {
+                console.log('this year you will have blast!, BUT this month', today.getMonth() + 1,'wait till ',theirDate.getMonth() +1);
+                if(today.getMonth() - theirDate.getMonth() == 0) {
+                    console.log('almost ! ');
+                    if(today.getDate() - theirDate.getDate() < 0) {
+                        console.log('few more days ! ');
+                        $timeout(function() {
+                            $location.path('/age-gate-block'); 
+                        }, 1000);
+                    } else {
+                        console.log('hooray! go get your drink');
+                        $timeout(function() {
+                            $location.path('/landing'); 
+                        }, 1000);
+                    }
+                } else if(today.getMonth() - theirDate.getMonth() < 0){
+                    console.log('few more months ! ');
+                    $timeout(function() {
+                        $location.path('/age-gate-block'); 
+                    }, 1000);
+                }
+            } else {
+                console.log("hooray! go get your drink");
+                $timeout(function() {
+                    $location.path('/redirecting'); 
+                }, 1000);
+            }
+        }
+
+        /* 
+         * submitting for DESKTOP from here , all comment should be deleted
+         */
+        // getting age - desktop
+        $scope.getAge = function(age, cName) {
+            // closing list 
+            $scope.listdown = 'false';
+            // selected country and age value
+            $scope.country = cName;
+            $scope.age = age;
+
+            console.log(age,'|',cName);
+        }
+
+        // submit agegate form - desktop
+        $scope.submitFormDesktop = function(bday) {
+            // getting user's birthday
+            var val = bday.split("-");
+
+            var year = parseInt(val[2]);
+            var month = parseInt(val[1] - 1);
+            var day = parseInt(val[0]);
+            var min_age = $scope.age;
+            
+            var userYear = parseInt(val[2]) + parseInt(min_age);
+
+            console.log('just user year +++++', userYear);
+            console.log('user bday y/m/d', val[2],val[1],val[0]);
+
+            var theirDate = new Date((userYear), month, day);
+            var today = new Date;
+            var thisYear = today.getYear() + 1900;
+
+            console.log('their legal date',new Date((userYear), month, day));
+
+            console.log('available year : ', parseInt(val[2]) + parseInt(min_age));
+
+            if((today.getYear() - theirDate.getYear()) < 0 ) {
+                console.log('no alcohol');
+                $timeout(function() {
+                    $location.path('/age-gate-block'); 
+                }, 1000);
+            } else if (today.getYear() - theirDate.getYear() == 0 ) {
+                console.log('this year you will have blast!, BUT', today.getMonth() + 1,theirDate.getMonth() +1);
+                if(today.getMonth() - theirDate.getMonth() == 0) {
+                    console.log('almost !!!!');
+                    if(today.getDate() - theirDate.getDate() < 0) {
+                        console.log('few more days ! ');
+                        $timeout(function() {
+                            $location.path('/age-gate-block'); 
+                        }, 1000);
+                    } else {
+                        console.log('hooray! go get your drink');
+                        $timeout(function() {
+                            $location.path('/landing'); 
+                        }, 1000);
+                    }
+                } else if(today.getMonth() - theirDate.getMonth() < 0){
+                    console.log('few more months ! ');
+                    $timeout(function() {
+                        $location.path('/age-gate-block'); 
+                    }, 1000);
+                }
+            } else {
+                console.log("hooray! go get your drink");
+                $timeout(function() {
+                    $location.path('/redirecting'); 
+                }, 1000);
+            }
+        }        
+    }
+])
+.controller('TestViewCtrl', ['$scope', 
+    function($scope) {
+        console.log('TestViewCtrl');
     }
 ]);
